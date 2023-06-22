f661bcca3ffe88d93a4cbebeff669e89af5e3399
Carlos Antonio Portillo Di Nobile <113128657+Motsito@users.noreply.github.com>
2023-06-21 21:42:31 -0600

    bug fix
    second click wasn't getting applied properly

--- src/Components/Months/Months.js
+++ src/Components/Months/Months.js
@@ -1,258 +1,241 @@
-import {React, useState, useEffect, useContext} from 'react'
+import { React, useState, useEffect, useContext } from "react";
 import "./Months.css";
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import GlobalContext from "../../context/GlobalContext"
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import GlobalContext from "../../context/GlobalContext";
 
 export default function Months() {
+  const {
+    appointmentsList,
+    selectedDay,
+    currentMonth,
+    date,
+    firstDay,
+    setFirstDay,
+    currentYear,
+    setCurrentYear,
+    setCurrentMonth,
+    setSelectedDay,
+    setShowModal,
+    showModal,
+  } = useContext(GlobalContext);
 
-   const { 
-      appointmentsList, 
-      showModal, 
-      selectedDay,
-      currentMonth,
-      date,
-      firstDay,
-      setFirstDay,
-      currentYear,
-      setCurrentYear,
-      setCurrentMonth,
-      setSelectedDay,
-      setShowModal
-   } = useContext(GlobalContext);
+  useEffect(() => {
+    if (showModal === false) {
+      setSelectedDay(null);
+    }
+  }, [showModal]);
+  // months information
 
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-   // months information
+  const [monthboxes, setMonthBoxes] = useState(
+    Array.from({ length: 42 }, (_, i) => ({
+      id: i + 1,
+    }))
+  );
+  const boxClick = (index, currentBox, currentDays) => {
+    //if that allow us to open the modal only when second click gets recieved
+    let isItTrue = selectedDay === index;
 
-   const months = [
-      'January', 
-      'February', 
-      'March', 
-      'April', 
-      'May', 
-      'June', 
-      'July', 
-      'August', 
-      'September', 
-      'October', 
-      'November', 
-      'December'
-   ];
+    if (!!isItTrue) {
+      setShowModal(true);
+    }
 
-   const [monthboxes, setMonthBoxes] = useState(Array.from(
-      { length: 42 }, (_, i) => (
-         { 
-            id: i + 1
-         }
-      )))
-      const [clickCount, setClickCount] = useState(0);
-// key listeners for the user to move between days
+    //if that allow us to select only days with white background
+    if (index >= firstDay && currentBox <= currentDays) {
+      setSelectedDay(index);
+    } else {
+      setShowModal(false);
+      return;
+    }
+  };
 
-   useEffect(() => {
-      const changeDayWithKey = (event) => {
-         if (event.key === 'ArrowRight') {
-            setSelectedDay((prevNumber) => prevNumber + 1);
-         } else if (event.key === 'ArrowLeft') {
-            setSelectedDay((prevNumber) => prevNumber - 1);
-         } else if (event.key === 'ArrowUp') {
-            setSelectedDay((prevNumber) => prevNumber - 7)
-         } else if (event.key === 'ArrowDown') {
-            setSelectedDay((prevNumber) => prevNumber + 7)
-         }else {
-            return
-         }
-      };
+  //function that gives ammount of days based in date methods
 
-      window.addEventListener('keydown', changeDayWithKey);
+  const getAmmountDays = (year, month) => {
+    const firstDayOfNextMonth = new Date(year, month + 1, 1);
+    const lastDayOfMonth = new Date(firstDayOfNextMonth - 1);
+    const daysInMonth = lastDayOfMonth.getDate();
+    return daysInMonth;
+  };
 
-      return () => {
-         window.removeEventListener('keydown', changeDayWithKey);
-      };
-   }, []);
+  // functions that allow next and back button change month
 
-   const boxClick = (index, currentBox,currentDays) => {
+  const pastMonth = () => {
+    if (currentMonth > 0 && currentMonth <= 11) {
+      setCurrentMonth((prev) => {
+        return prev - 1;
+      });
+      setFirstDay(new Date(currentYear, currentMonth - 1, 1).getDay());
+      setSelectedDay(null);
+    } else if (currentMonth === 0) {
+      setCurrentMonth(11);
+      setCurrentYear((prev) => prev - 1);
+      setFirstDay(new Date(currentYear - 1, 11, 1).getDay());
+      setSelectedDay(null);
+    }
+  };
 
-      if (clickCount === 1) {
-         setShowModal(true);
-      } else if (clickCount > 1 || index !== setSelectedDay) {
-         setClickCount(0);
-         setShowModal(false)
-      }
-      if(index >= firstDay && currentBox <= currentDays ){
-         setSelectedDay(index);
-      }else{
-         setClickCount(0);
-         setShowModal(false)
-         return 
-      }
+  const nextMonth = () => {
+    console.log(appointmentsList);
+    if (currentMonth >= 0 && currentMonth <= 10) {
+      setCurrentMonth((prev) => {
+        return prev + 1;
+      });
+      setFirstDay(new Date(currentYear, currentMonth + 1, 1).getDay());
+      setSelectedDay(null);
+    } else if (currentMonth === 11) {
+      setCurrentMonth(0);
+      setCurrentYear((prev) => prev + 1);
+      setFirstDay(new Date(currentYear + 1, 0, 1).getDay());
+      setSelectedDay(null);
+    }
+  };
 
-   setClickCount((prev)=>prev + 1);
+  //function that will add the needed class for the specific day
 
-      
-   };
+  const getClassNames = (
+    dayIndex,
+    currentBox,
+    currentDays,
+    notAvailableDaysHelper
+  ) => {
+    const classNames = {
+      boxContent: true,
+      grayBox: notAvailableDaysHelper,
+      currentDay: selectedDay === dayIndex,
+      today:
+        date.getDate() === currentBox - firstDay &&
+        date.getMonth() === currentMonth &&
+        date.getFullYear() === currentYear,
+    };
 
-   //function that gives ammount of days based in date methods
+    return Object.keys(classNames)
+      .filter((className) => classNames[className])
+      .join(" ");
+  };
 
-   const getAmmountDays = (year, month) => {
-      const firstDayOfNextMonth = new Date(year, month + 1, 1);
-      const lastDayOfMonth = new Date(firstDayOfNextMonth - 1);
-      const daysInMonth = lastDayOfMonth.getDate();
-      return daysInMonth
-   }
-   
-   // functions that allow next and back button change month
+  //function that will add the needed number for the specific day
 
-   const pastMonth = () => {
-      if(currentMonth > 0 && currentMonth <= 11){
-         setCurrentMonth((prev)=>{return prev - 1})
-         setFirstDay(new Date(currentYear, currentMonth - 1, 1).getDay())
-         setSelectedDay(null)
-      }else if (currentMonth === 0){
-         setCurrentMonth(11)
-         setCurrentYear((prev) => prev - 1)
-         setFirstDay(new Date(currentYear - 1, 11, 1).getDay())
-         setSelectedDay(null)
-      }
-   }
-
-   const nextMonth = () => {
-      console.log(appointmentsList)
-      if(currentMonth >= 0 && currentMonth <= 10){
-         setCurrentMonth((prev)=>{return prev + 1})
-         setFirstDay(new Date(currentYear, currentMonth + 1, 1).getDay())
-         setSelectedDay(null)
-      }else if (currentMonth === 11){
-         setCurrentMonth(0)
-         setCurrentYear((prev) => prev + 1)
-         setFirstDay(new Date(currentYear + 1, 0, 1).getDay())
-         setSelectedDay(null)
-      }
-   }
-
-
-
-//function that will add the needed class for the specific day
-
-   const getClassNames = (dayIndex, currentBox, currentDays, notAvailableDaysHelper) => {
-      const classNames = {
-         boxContent: true,
-         grayBox: notAvailableDaysHelper,
-         currentDay : selectedDay === dayIndex,
-         today: date.getDate() === currentBox - firstDay && date.getMonth() === currentMonth && date.getFullYear() === currentYear
-   }
-
-   return Object.keys(classNames)
-   .filter((className) => classNames[className])
-   .join(" ");
-}
-
-//function that will add the needed number for the specific day
-
-const getDayNumber = (index, 
-   currentBox, 
-   currentDays,
-   pastMonthDays
-   ) => {
-   if(currentBox <= firstDay){
+  const getDayNumber = (index, currentBox, currentDays, pastMonthDays) => {
+    if (currentBox <= firstDay) {
       return pastMonthDays - firstDay + currentBox;
-      }else if(currentBox > firstDay && currentBox <= (currentDays+firstDay)){
-         return currentBox - firstDay
-      }else if (currentBox > (currentDays+firstDay)){
-         return currentBox - firstDay - currentDays
+    } else if (currentBox > firstDay && currentBox <= currentDays + firstDay) {
+      return currentBox - firstDay;
+    } else if (currentBox > currentDays + firstDay) {
+      return currentBox - firstDay - currentDays;
+    }
+    return currentBox;
+  };
+  //function that retrieves appointments info
+  const getAppointments = (index, day, month) => {
+    return appointmentsList.map((task) => {
+      let taskDate = new Date(task.time);
+      let taskMonth = taskDate.getMonth();
+      let taskDay = taskDate.getDate();
+      if (taskMonth === month && taskDay === day) {
+        return (
+          <span className="dayText" key={Math.floor(Math.random() * index)}>
+            {task.name}
+          </span>
+        );
       }
-   return currentBox
-}
-   //function that retrieves appointments info
-   const getAppointments = (index,day,month) => {
-      return appointmentsList.map((task) => {
-         let taskDate = new Date(task.time);
-         let taskMonth = taskDate.getMonth();
-         let taskDay = taskDate.getDate();
-         if(taskMonth === month && taskDay === day){
-            return (<span className='dayText' key={Math.floor(Math.random() * index)}>{task.name}</span>)
-         }
-      })
-   }
+    });
+  };
 
-//********************          tryall             ********************
-
-const makeDays = () =>{
-   return monthboxes.map((_, index)=>{
+  const makeDays = () => {
+    return monthboxes.map((_, index) => {
       let currentBox = index + 1;
       let currentDays = getAmmountDays(currentYear, currentMonth);
-      let notAvailableDaysHelper = (currentBox <= firstDay) || currentBox < firstDay || currentBox > (currentDays + firstDay)
-      let pastMonthDays = () =>{
-         if(currentMonth > 0 && currentMonth <= 11){
-            return getAmmountDays(currentYear, currentMonth - 1)
-         }else if (currentMonth === 0){
-            return getAmmountDays(currentYear - 1, 11)
-         }
-      }
-      return(
-         <div className={
-            getClassNames(
-               index, 
-               currentBox, 
-               currentDays, 
-               notAvailableDaysHelper
-            )} 
-            key={index} 
-            onClick={()=>{
-               boxClick(
-                  index,
-                  currentBox - firstDay,
-                  currentDays)}
-            }
-         >
-         <p>{getDayNumber(
-               index, 
-               currentBox, 
-               currentDays,
-               pastMonthDays(),
-            )}
-         </p>
-         <div>
-               {
-                  getAppointments(index, currentBox - firstDay, currentMonth)
-               }
-         </div>
-      </div>
-      )
-   })
+      const notAvailableDaysHelper =
+        currentBox <= firstDay ||
+        currentBox < firstDay ||
+        currentBox > currentDays + firstDay;
+      let pastMonthDays = () => {
+        if (currentMonth > 0 && currentMonth <= 11) {
+          return getAmmountDays(currentYear, currentMonth - 1);
+        } else if (currentMonth === 0) {
+          return getAmmountDays(currentYear - 1, 11);
+        }
+      };
+      return (
+        <div
+          className={getClassNames(
+            index,
+            currentBox,
+            currentDays,
+            notAvailableDaysHelper
+          )}
+          key={index}
+          onClick={() => {
+            boxClick(index, currentBox - firstDay, currentDays);
+          }}
+        >
+          <p>{getDayNumber(index, currentBox, currentDays, pastMonthDays())}</p>
+          <div>
+            {getAppointments(index, currentBox - firstDay, currentMonth)}
+          </div>
+        </div>
+      );
+    });
+  };
 
-}
-
-   return (
-      <div className='borderGrid my-3'> 
-         <div>
-            <Row className='text-center mb-2'>
-               <Col xs>
-                  <Button variant="primary border" size="lg" className='px-5 py-2' onClick={()=>pastMonth()}>
-                     {`<`}
-                  </Button>
-               </Col>
-               <Col>
-                  <h2>{months[currentMonth]}  {currentYear}</h2>
-               </Col>
-               <Col xs>
-                  <Button variant="primary border" size="lg" className='px-5 py-2' onClick={()=>nextMonth()}>
-                     {`>`}
-                  </Button>
-               </Col>
-               
-            </Row>
-         </div>
-         <div className='grid'>
-            <p className='dayBox'>Sun</p>
-            <p className='dayBox'>Mon</p>
-            <p className='dayBox'>Tue</p>
-            <p className='dayBox'>Wed</p>
-            <p className='dayBox'>Thu</p>
-            <p className='dayBox'>Fri</p>
-            <p className='dayBox'>Sat</p>
-            {makeDays()}
-         </div>
+  return (
+    <div className="borderGrid my-3">
+      <div>
+        <Row className="text-center mb-2">
+          <Col xs>
+            <Button
+              variant="primary border"
+              size="lg"
+              className="px-5 py-2"
+              onClick={() => pastMonth()}
+            >
+              {`<`}
+            </Button>
+          </Col>
+          <Col>
+            <h2>
+              {months[currentMonth]} {currentYear}
+            </h2>
+          </Col>
+          <Col xs>
+            <Button
+              variant="primary border"
+              size="lg"
+              className="px-5 py-2"
+              onClick={() => nextMonth()}
+            >
+              {`>`}
+            </Button>
+          </Col>
+        </Row>
       </div>
-   )
+      <div className="grid">
+        <p className="dayBox">Sun</p>
+        <p className="dayBox">Mon</p>
+        <p className="dayBox">Tue</p>
+        <p className="dayBox">Wed</p>
+        <p className="dayBox">Thu</p>
+        <p className="dayBox">Fri</p>
+        <p className="dayBox">Sat</p>
+        {makeDays()}
+      </div>
+    </div>
+  );
 }
--- src/Modal/EventModal.js
+++ src/Modal/EventModal.js
@@ -1,151 +1,198 @@
-import React, { useState, useContext } from 'react';
-import { Button, Modal, Form} from 'react-bootstrap';
-import GlobalContext from '../context/GlobalContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark} from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useContext } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import GlobalContext from "../context/GlobalContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function EventModal(props) {
-   const {
-      appointmentsList, 
-      showModal, 
-      selectedDay,
-      currentMonth,
-      firstDay,
-      currentYear,
-      setAppointmentsList,
-      setShowModal
-   } = useContext(GlobalContext); 
+  const {
+    appointmentsList,
+    showModal,
+    selectedDay,
+    currentMonth,
+    firstDay,
+    currentYear,
+    setAppointmentsList,
+    setShowModal,
+  } = useContext(GlobalContext);
 
-   //show modal
-   const [notTimeSelected, setNotTimeSelected] = useState(null)
-   const [addApBol, setAddApBol] = useState(false)
-   const closeModal = () =>{
-      setAddApBol(false);
-      setShowModal(false);
-      setNotTimeSelected(null)
-   }
+  //show modal
+  const [notTimeSelected, setNotTimeSelected] = useState(null);
+  const [addApBol, setAddApBol] = useState(false);
+  const closeModal = () => {
+    setAddApBol(false);
+    setShowModal(false);
+    setNotTimeSelected(null);
+  };
 
-   //appointment settings
-   const [lastAppointment, setLastAppointment] = useState(null)
-   const [selectedTime, setSelectedTime] = useState(null);
+  //appointment settings
+  const [lastAppointment, setLastAppointment] = useState(null);
+  const [selectedTime, setSelectedTime] = useState(null);
 
-   const getAppointments = (day, month) => {
-      const dayAppointments = appointmentsList.filter(
-         (task) => new Date(task.time).getDate() === day && new Date(task.time).getMonth() === month
-      ).sort((a, b) => {
-         return new Date(a.time) - new Date(b.time);
+  const getAppointments = (day, month) => {
+    const dayAppointments = appointmentsList
+      .filter(
+        (task) =>
+          new Date(task.time).getDate() === day &&
+          new Date(task.time).getMonth() === month
+      )
+      .sort((a, b) => {
+        return new Date(a.time) - new Date(b.time);
       });
-      
-      console.log(dayAppointments)
-      return dayAppointments.map((task, index) => {
-         let taskDate = new Date(task.time);
-         let getHours = taskDate.getHours() < 10 ? `0${taskDate.getHours()}` : taskDate.getHours();
-         let getMinutes = taskDate.getMinutes() < 10 ? `0${taskDate.getMinutes()}` : taskDate.getMinutes();
-         let taskTime = getHours + ":" + getMinutes;
-         return (
-            <div className='onModalDayText px-2' key={Math.floor(Math.random() * index)}>
-            <span>{task.name}</span>
-            <div>
-               <span>{taskTime} </span>
-               <Button className='mb-1 ms-2' variant="danger" size="sm" onClick={() => {deleteAppointment(task.name)}}>
-                  <FontAwesomeIcon icon={faXmark} size="lg" />
-               </Button>
-            </div>
-            </div>
-         )
-      })
-   }
-      
 
-   //function that allows user delete appointments
-
-   const deleteAppointment = (name) =>{
-      const newList = appointmentsList.filter((appointment)=>appointment.name !== name);
-      setAppointmentsList(newList)
-   }
-
-   const addTitle = () => {
-      setLastAppointment(prompt('Enter new appointment:'))
-   };
-
-   const addAppointment = () => {
-      setAddApBol(true)
-   }
-
-   const saveAppointment = () => {
-      if(addApBol===true){
-         if(selectedTime){
-            let correctedDayBox = selectedDay - firstDay + 1
-            let [hours, minutes] = selectedTime.split(":");
-            let isoTime = new Date(currentYear, currentMonth, correctedDayBox, hours, minutes).toISOString();
-            let newDate = {time: isoTime , name: lastAppointment}
-            const updatedAppointmentsList = [...appointmentsList, newDate];
-            setAppointmentsList(updatedAppointmentsList);
-            props.onSave(updatedAppointmentsList);
-            setLastAppointment(null);
-            setSelectedTime(null);
-            setShowModal(false);
-            setAddApBol(false);
-            setNotTimeSelected(false);
-            localStorage.setItem('appointmentsList', JSON.stringify(updatedAppointmentsList));
-         }else{
-            setNotTimeSelected(true)
-         }
-
-      }else{
-         setShowModal(false);
-         setAddApBol(false);
-         const updatedAppointmentsList = [...appointmentsList];
-         props.onSave(updatedAppointmentsList);
-         localStorage.setItem('appointmentsList', JSON.stringify(updatedAppointmentsList));
-      }
-
-   };
-
-
-   return (
-         <>
-         <Modal show={showModal} onHide={closeModal}>
-            <Modal.Header closeButton>
-               <Modal.Title><h1>Today To Do's:</h1></Modal.Title>
-            </Modal.Header>
-
-            <Modal.Body>
-            <div className='my-3 mx-3'>
-                  {
-                     getAppointments(selectedDay - 2, currentMonth)
-                  }
-            </div>
-
-         {addApBol === true ? 
-            <>
-               <div className='d-flex justify-content-between w-100 mt-4 mb-3'>
-                  {lastAppointment ? <h4>{lastAppointment} </h4> : <h4>Date today?</h4> }
-                  <Form.Control className='w-50' size="lg" type="time" onChange={(event)=>{setSelectedTime(event.target.value)}} value={selectedTime || ''} />
-               </div>
-               <Button className='ms-2' variant="secondary" onClick={addTitle}>
-                  Add Title
-               </Button>
-               {notTimeSelected === true ? <p className="alert alert-danger text-sm my-3 p-2">Select a time.</p> : <></>}
-            </>:
-            <Button variant="secondary" onClick={()=>addAppointment()}>
-               Add Another
+    return dayAppointments.map((task, index) => {
+      let taskDate = new Date(task.time);
+      let getHours =
+        taskDate.getHours() < 10
+          ? `0${taskDate.getHours()}`
+          : taskDate.getHours();
+      let getMinutes =
+        taskDate.getMinutes() < 10
+          ? `0${taskDate.getMinutes()}`
+          : taskDate.getMinutes();
+      let taskTime = getHours + ":" + getMinutes;
+      return (
+        <div
+          className="onModalDayText px-2"
+          key={Math.floor(Math.random() * index)}
+        >
+          <span>{task.name}</span>
+          <div>
+            <span>{taskTime} </span>
+            <Button
+              className="mb-1 ms-2"
+              variant="danger"
+              size="sm"
+              onClick={() => {
+                deleteAppointment(task.name);
+              }}
+            >
+              <FontAwesomeIcon icon={faXmark} size="lg" />
             </Button>
-            }
-
-            </Modal.Body>
-
-            <Modal.Footer>
-               <Button variant="secondary" onClick={closeModal}>
-               Cancel
-               </Button>
-               <Button variant="primary" onClick={saveAppointment}>
-               Save
-               </Button>
-            </Modal.Footer>
-         </Modal>
-         </>
+          </div>
+        </div>
       );
-   }
+    });
+  };
 
-   export default EventModal;
+  //function that allows user delete appointments
+
+  const deleteAppointment = (name) => {
+    const newList = appointmentsList.filter(
+      (appointment) => appointment.name !== name
+    );
+    setAppointmentsList(newList);
+  };
+
+  const addTitle = () => {
+    setLastAppointment(prompt("Enter new appointment:"));
+  };
+
+  const addAppointment = () => {
+    setAddApBol(true);
+  };
+
+  const saveAppointment = () => {
+    if (addApBol === true) {
+      if (selectedTime) {
+        let correctedDayBox = selectedDay - firstDay + 1;
+        let [hours, minutes] = selectedTime.split(":");
+        let isoTime = new Date(
+          currentYear,
+          currentMonth,
+          correctedDayBox,
+          hours,
+          minutes
+        ).toISOString();
+        let newDate = { time: isoTime, name: lastAppointment };
+        const updatedAppointmentsList = [...appointmentsList, newDate];
+        setAppointmentsList(updatedAppointmentsList);
+        props.onSave(updatedAppointmentsList);
+        setLastAppointment(null);
+        setSelectedTime(null);
+        setShowModal(false);
+        setAddApBol(false);
+        setNotTimeSelected(false);
+        localStorage.setItem(
+          "appointmentsList",
+          JSON.stringify(updatedAppointmentsList)
+        );
+      } else {
+        setNotTimeSelected(true);
+      }
+    } else {
+      setShowModal(false);
+      setAddApBol(false);
+      const updatedAppointmentsList = [...appointmentsList];
+      props.onSave(updatedAppointmentsList);
+      localStorage.setItem(
+        "appointmentsList",
+        JSON.stringify(updatedAppointmentsList)
+      );
+    }
+  };
+
+  return (
+    <>
+      <Modal show={showModal} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <h1>Today To Do's:</h1>
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <div className="my-3 mx-3">
+            {getAppointments(selectedDay - 2, currentMonth)}
+          </div>
+
+          {addApBol === true ? (
+            <>
+              <div className="d-flex justify-content-between w-100 mt-4 mb-3">
+                {lastAppointment ? (
+                  <h4>{lastAppointment} </h4>
+                ) : (
+                  <h4>Date today?</h4>
+                )}
+                <Form.Control
+                  className="w-50"
+                  size="lg"
+                  type="time"
+                  onChange={(event) => {
+                    setSelectedTime(event.target.value);
+                  }}
+                  value={selectedTime || ""}
+                />
+              </div>
+              <Button className="ms-2" variant="secondary" onClick={addTitle}>
+                Add Title
+              </Button>
+              {notTimeSelected === true ? (
+                <p className="alert alert-danger text-sm my-3 p-2">
+                  Select a time.
+                </p>
+              ) : (
+                <></>
+              )}
+            </>
+          ) : (
+            <Button variant="secondary" onClick={() => addAppointment()}>
+              Add Another
+            </Button>
+          )}
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={saveAppointment}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default EventModal;
